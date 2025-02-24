5c493567cb2e1ea0a78ad7bbfd4c9c14aaeb1a53
Anh Quan Nguyen <forwork.quanna@gmail.com>
2025-02-24 23:32:49 +0700

    heap - continue

--- src/DataStructures/heap/Heap.java
+++ src/DataStructures/heap/Heap.java
@@ -15,9 +15,9 @@ public class Heap {
         heap.add(val);
         int i = (heap.size() - 1);
 
-        while (i > 1 && heap.get(i/2) > val) {
-            int tmp = heap.get(i/2);
-            heap.set(i/2, val);
+        while (i > 1 && heap.get(i / 2) > val) {
+            int tmp = heap.get(i / 2);
+            heap.set(i / 2, val);
             heap.set(i, tmp);
 
             i = i / 2;
@@ -26,24 +26,24 @@ public class Heap {
 
     public int pop() {
         if (heap.size() == 1) {
-            //return null;
+            // return null;
         }
         if (heap.size() == 2) {
             return heap.remove(heap.size() - 1); // equivalent to heap.remove(1)
         }
-            
+
         // Get the first element (the root) to return
         int res = heap.get(1);
-        
+
         // Move last value to root
         heap.set(1, heap.remove(heap.size() - 1));
         int i = 1;
-        
+
         // Percolate down
-        while(2 * i < heap.size()) {  // while the root has the left child
-            if (2 * i + 1 < heap.size() &&  // if the root also has the right child
-            heap.get(2 * i + 1) < heap.get(2 * i) &&  // and the right child is smaller than the left child
-            heap.get(i) > heap.get(2 * i + 1)) {  // and the root is greater than the right child
+        while (2 * i < heap.size()) { // while the root has the left child
+            if (2 * i + 1 < heap.size() && // if the root also has the right child
+                    heap.get(2 * i + 1) < heap.get(2 * i) && // and the right child is smaller than the left child
+                    heap.get(i) > heap.get(2 * i + 1)) { // and the root is greater than the right child
                 // Swap right child
                 int tmp = heap.get(i);
                 heap.set(i, heap.get(2 * i + 1));
@@ -59,7 +59,41 @@ public class Heap {
                 break;
             }
         }
-        
+
         return res;
+    }
+
+    public void heapify(ArrayList<Integer> arr) {
+        // 0-th position is moved to the end
+        arr.add(arr.get(0));
+
+        heap = arr;
+        int cur = (heap.size() - 1) / 2;
+        while (cur > 0) {
+            // Percolate Down
+            int i = cur;
+            while (2 * i < heap.size()) {
+                if (2 * i + 1 < heap.size() &&
+                    heap.get(2 * i + 1) < heap.get(2 * i) &&
+                    heap.get(i) > heap.get(2 * i + 1)
+                ) {
+                    // Swap right child
+                    int tmp = heap.get(i);
+                    heap.set(i, heap.get(2 * i + 1));
+                    heap.set(2 * i + 1, tmp);
+                    i = 2 * i + 1;
+                } else if (heap.get(i) > heap.get(2 * i)) {
+                    // Swap left child
+                    int tmp = heap.get(i);
+                    heap.set(i, heap.get(2 * i));
+                    heap.set(2 * i, tmp);
+                    i = 2 * i;
+                } else {
+                    break;
+                }
+            }
+            cur--;
+        }
+        return;
     }
 }
